a327869da34f7689d7ce2f34706aac876b224a5a
Chip Morgan <python.chipmorgan@gmail.com>
2022-09-28 08:19:58 -0500

    add if and else if statement to js file

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,8 +5,44 @@ function getComputerChoice (min, max) {
 var computerSelection = getComputerChoice(1,3);
 console.log(computerSelection);
 
-var playerSelection = readLine("Type rock, scissors, or paper.");
+var playerSelection = prompt("Type rock, scissors, or paper.");
 console.log(playerSelection);
 
-//function playRound(playerSelection, computerSelection)//
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === 1){
+        return("It's a tie. Rock equals rock.");
+    }else if (playerSelection === "rock" && computerSelection === 2){
+        return("You lose! Paper beats rock.");
+    }else if (playerSelection === "rock" && computerSelection === 3){
+        return("You win! Rock beats scissors.");
+    }
+    else if (playerSelection === "paper" && computerSelection === 1){
+        return("You win! Paper beats rock.");
+    }else if (playerSelection === "paper" && computerSelection === 2){
+        return("It's a tie. Paper equals paper.");
+    }else if (playerSelection === "paper" && computerSelection === 3){
+        return("You lose! Scissors beat paper.");
+    }
+    else if (playerSelection === "scissors" && computerSelection === 1){
+        return("You lose! Rock beats scissors.");
+    }else if (playerSelection === "scissors" && computerSelection === 2){
+        return("You win! Scissors beat paper.");
+    }else if (playerSelection === "scissors" && computerSelection === 3){
+        return("It's a tie. Scissors equals scissors.");
+    }
+}
+
+//alert(playRound(playerSelection, computerSelection));//
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+        
+    }
+}
+
+game()
+
+
+
 
